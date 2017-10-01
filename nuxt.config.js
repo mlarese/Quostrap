92d305a859553b3f8e1fe7ec249d127b9d2c397f
Courage <acourage91@gmail.com>
2017-10-01 13:39:24 +0000

    upload

---
+++ nuxt.config.js
@@ -0,0 +1,49 @@
+const webpack = require('webpack')
+module.exports = {
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: 'quostrap',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: 'Nuxt.js project' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+  /*
+  ** Customize the progress bar color
+  */
+  loading: { color: '#3B8070' },
+  /*
+  ** Build configuration
+  */
+  css: ['bootstrap/dist/css/bootstrap.css'],
+  plugins: ['~plugins/bootstrap.js'],
+  build: {
+    vendor: ['jquery', 'bootstrap'],
+    plugins: [
+      new webpack.ProvidePlugin({
+        $: 'jquery',
+        jQuery: 'jquery',
+        'window.jQuery': 'jquery'
+      })
+    ],
+    /*
+    ** Run ESLint on save
+    */
+    extend (config, ctx) {
+      if (ctx.dev && ctx.isClient) {
+        config.module.rules.push({
+          enforce: 'pre',
+          test: /\.(js|vue)$/,
+          loader: 'eslint-loader',
+          exclude: /(node_modules)/
+        })
+      }
+    }
+  }
+}
---
+++ pages/test.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <div class="row">
+            <div class="col-xs-6" style="background-color:red">
+                red
+            </div>
+            <div class="col-xs-6" style="background-color:yellow">
+                yellow
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+  export default {
+    components: {},
+    props: []
+  }
+</script>
+
+<style>
+
+</style>
