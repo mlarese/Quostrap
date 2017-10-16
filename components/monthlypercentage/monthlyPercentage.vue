f33168f6f4d529538f3b9dc71a58e774ca771c47
maurolarese <asemota91>
2017-10-16 20:04:54 +0200

    update

--- components/monthlypercentage/monthlyPercentage.vue
+++ components/monthlypercentage/monthlyPercentage.vue
@@ -5,7 +5,6 @@
             <a href="#" class="text-muted pull-right"><em class="fa fa-arrow-right"></em></a>
             <div class="text-info">{{$t('Average Monthly Uploads')}}</div>
             <div class="text-center pv-xl">
-                <div data-size="145" data-line-cap="round" data-line-width="10" data-scale-color="false" data-track-color="rgba(200,200,200,0.4)" data-bar-color="#23b7e5" data-animate="{\"duration\":="" \"800\",="" \"enabled\":="" \"true\"}"="" data-percent="70" data-easypiechart="" class="easypie-chart easypie-chart-lg">
                 <span>{{percentage}}</span>
             </div>
         </div>
