1955c707e06ecef1dbe463f3a10a963c38ae3e0c
maurolarese <asemota91>
2017-10-16 18:29:22 +0200

    update

--- components/descriptionuser/DescriptionUser.vue
+++ components/descriptionuser/DescriptionUser.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="panel widget" :style="{backgroundColor}">
         <div class="row row-table">
-            <div class="col-xs-4 text-center pv-lg" :style="{backgroundColor1}">
+            <div class="col-xs-4 text-center pv-lg" :style="{backgroundColor:backgroundColorDark}">
                 <em class="icon-globe fa-3x"></em>
             </div>
             <div class="col-xs-8 pv-lg"><div class="h2 mt0" :style="{color}">{{value}}
@@ -16,8 +16,8 @@
 <script>
   export default {
     props: {
-      backgroundColor: {default: 'yellow'},
-      backgroundColor1: {default: '#564aa3'},
+      backgroundColor: {default: 'red'},
+      backgroundColorDark: {default: '#564aa3'},
       color: {default: 'white'},
       value: {default: 7257},
       labelIfAny: {default: 'TB'},
